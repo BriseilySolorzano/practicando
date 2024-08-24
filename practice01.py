56abff920215c655875997358416c685eb3015d1
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-08-23 19:08:54 -0600

    #prueba03

--- practice01.py
+++ practice01.py
@@ -1,7 +1,7 @@
 
 for i in range(1,5):
     print("Hello world")
-    op=input("Desea continuar S/N: ")
+    op=input("Desea continuar S/N: ").lower()
     if op == "S":
         continue
     else:
